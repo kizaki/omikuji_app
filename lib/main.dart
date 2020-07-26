12a6f6e0e1e2fb72708d407b325f0fa7a0694a69
kizaki <kizaki.aiit@gmail.com>
2020-07-26 20:26:42 +0900

    ・アイコン名変更 ・ランダム関数を利用したおみくじロジック追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -31,35 +33,52 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Image image; // 中央に表示する画像
+  String imageName = "images/top.png";
+  var random = new math.Random();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: new Stack(children: <Widget>[
-        /*
         new Image.asset(
-          "images/top.png",
-          fit: BoxFit.fitWidth, // 横幅を画面に合わせる
+          "$imageName",
+          //fit: BoxFit.fitHeight, // 縦幅を画面に合わせる
           alignment: new Alignment(0.0, -0.5),
-          //alignment: AlignmentDirectional.topCenter, // 中央上部に配置
         ),
-        */
-        image = new Image.asset("images/top.png",
-            fit: BoxFit.fitWidth, alignment: new Alignment(0.0, -0.5)),
         new Align(
-          alignment: new Alignment(0.0, 0.9),
+          alignment: new Alignment(0.0, 0.8),
           child: Container(
             margin: new EdgeInsets.only(bottom: 0.0),
             child: new RaisedButton(
               onPressed: () {
                 setState(() {
-                  // ここにボタンがをされたあとの処理をかく
-
-                  image = new Image.asset("images/daikichi.png",
-                      fit: BoxFit.fitWidth,
-                      alignment: new Alignment(0.0, -0.5));
-                });
+                  int kazu = random.nextInt(7);
+                  print(kazu);
+                    switch (kazu){
+                      case 0:
+                        imageName = "images/daikichi.png";
+                        break;
+                      case 1:
+                        imageName = "images/chuukichi.png";
+                        break;
+                      case 2:
+                        imageName = "images/daikyou.png";
+                        break;
+                      case 3:
+                        imageName = "images/kichi.png";
+                        break;
+                      case 4:
+                        imageName = "images/kyou.png";
+                        break;
+                      case 5:
+                        imageName = "images/suekichi.png";
+                        break;
+                      case 6:
+                        imageName = "images/syoukichi.png";
+                        break;
+                    }
+                  });
               },
               child: new Text(
                 "おみくじを引く",
